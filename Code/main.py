e5befa5f6f6cf46e248e8b633c1d09e321655eb6
Benjapied <115982769+Benjapied@users.noreply.github.com>
2023-04-14 15:27:46 +0200

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -53,19 +53,26 @@ class Obstacle (pygame.sprite.Sprite):
 
 
 class Game (object):
-
+    '''C'est la classe qui stock toutes les infos de la partie
+        elle se reset à chaque lancement du programme
+        elle contient: 
+        _une méthode pour faire spawner des ennemis
+        _une méthode pour faire spawn des obstacles
+        _une méthode pour vérifier les collisions
+    '''
     def __init__(self):
         self.all_players = pygame.sprite.Group()
-        self.all_players = pygame.sprite.Group()
         self.player = Player(self)
-       
         self.all_players.add(self.player)
+        
         self.all_monsters = pygame.sprite.Group()
         self.spawn_monster()
         self.spawn_monster()
+
         self.all_players.add(self.player)
         self.all_obstacles = pygame.sprite.Group()
         self.spawn_obstacle()
+        
         self.distance = 0
         self.distanceScore = 0
         self.totalScore = 0
@@ -73,14 +80,18 @@ class Game (object):
         #stocker les touches activées par le joueur 
         self.pressed = {}
 
+
     def check_collision(self, sprite, group):
+        '''méthode qui check les collisions, return liste des sprites dans group qui touchent le 'sprite' '''
         return pygame.sprite.spritecollide(sprite, group, False, pygame.sprite.collide_mask)
 
     def spawn_monster(self):
+        '''Instencie une objet monstre et le place dans une liste de tous les monstres'''
         monster = Monster(self)
         self.all_monsters.add(monster)
 
     def spawn_obstacle(self):
+        '''Instencie une objet obstacle et le place dans une liste de tous les obstacles'''
         obstacle = Obstacle(self)
         self.all_obstacles.add(obstacle)
 
