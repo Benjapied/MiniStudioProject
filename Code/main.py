63d3561f505122003acb56b6aa417cb2822116cc
BenjaminArhancet <barhancet@gaming.tech>
2023-04-17 14:37:55 +0200

    quelques commentaires

--- Code/main.py
+++ Code/main.py
@@ -112,18 +112,11 @@ class Game (object):
         self.all_bonus.add(bonus)
 
 
-#Classe du joueur principal
 class Player (pygame.sprite.Sprite):    
-    
+    '''classe qui créer le joueur principal'''
     def __init__ (self, game):
         super().__init__()
-        self.game = game
-        '''Methode d'initialisation'''
-        self.image = pygame.image.load("img/wazo.png")
-        self.rect = self.image.get_rect()
-        self.rect.x = 8
-        self.rect.y = 8
-
+        self.game = game #on met l'objet game dans toutes les classes pour qu'elles puissent interagir avec les autres objets
         self.velocity = 6 #vitesse du joueur
         self.attack = 10 #points d'attaque du joueur
         self.attack_speed = 1
@@ -132,47 +125,56 @@ class Player (pygame.sprite.Sprite):
         self.all_projectiles = pygame.sprite.Group()
         self.all_bonus = pygame.sprite.Group()
 
+        #Image et position
+        self.image = pygame.image.load("img/wazo.png")
+        self.rect = self.image.get_rect()
+        self.rect.x = 8
+        self.rect.y = 8
+
     def launch_projectile(self):
-        # créer une nouvelle instance de la classe projectile
+        '''créer une nouvelle instance de la classe projectile et la met dans la liste des projectiles'''
         self.all_projectiles.add(Projectile(self))
 
     def launch_elemental(self,element):
+        '''On va créer un projectile et lui mettre l'élément passé en parametre'''
         projectile = Projectile(self)
         projectile.element = element
         text  = "img/projectile_" + element + ".png"
-        print(text)
         projectile.image = pygame.image.load(text)
         projectile.image = pygame.transform.scale(projectile.image, (50, 50))
         self.all_projectiles.add(projectile)
 
 
     def moveDown(self):
+        '''déplace vers le bas, augmente le y'''
         self.rect.y = self.rect.y + self.velocity
 
     def moveUp (self) :
+        '''déplace vers le haut, diminue le y'''
         self.rect.y = self.rect.y - self.velocity 
     
     def moveLeft (self) :
+        '''déplace vers la gauche, diminue le x'''
         self.rect.x = self.rect.x - self.velocity
 
     def moveRight (self) :
-        #si le joueur n'est pas en collision
+        '''déplace vers la droite, augmente le x'''
         if not self.game.check_collision(self, self.game.all_monsters):
             self.rect.x += self.velocity 
 
 
 
-# définir la classe qui va gérer le projectile de notre joueur 
 class Projectile(pygame.sprite.Sprite):
-
-    #définir le constructeur de cette classe
+    '''Class qui crée un projectile à partir de la pos du joueur
+    ce projectile peut être d'un certain élément'''
     def __init__(self, player):
-        super().__init__()
+        super().__init__() #définit que la classe hérite d'une autre
         self.game = game
         self.velocity = 5
-        self.cycle = {"water":"fire","fire":"air","air":"earth","earth":"water"}
+        self.cycle = {"water":"fire","fire":"air","air":"earth","earth":"water"} #Dictionnaire qui répertorie les counter ({Element : sur quoi il est fort})
         self.player = player
         self.element = "neutral"
+        #Image et position
         self.image = pygame.image.load('img/projectile.png')
         self.image = pygame.transform.scale(self.image, (50, 50))
         self.rect = self.image.get_rect()
@@ -180,6 +182,7 @@ class Projectile(pygame.sprite.Sprite):
         self.rect.y = player.rect.y + 25
 
     def remove(self):
+        '''retire l'objet de la liste des projectiles'''
         self.player.all_projectiles.remove(self)
 
 
@@ -211,7 +214,7 @@ class Bonus(pygame.sprite.Sprite):
         self.game = game
         self.bonus_number = randint (1,1)
         self.text = "img/image_bonus_" + str(self.bonus_number)  # initialisation 
-        print(self.text)
+        #Image et position
         self.image = pygame.image.load(self.text +".png") # l'image du bonus
         self.image = pygame.transform.scale(self.image, (32, 32))
         self.rect = self.image.get_rect() #on définit la taille du bonus
