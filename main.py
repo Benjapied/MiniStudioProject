18e8f95a7c702196da91fc544ad284dba3dd8bdc
BlackDrift <fovet.vianney@gmail.com>
2023-04-23 16:12:39 +0200

    pause menu : adding buttons (replay/collec)

--- main.py
+++ main.py
@@ -21,9 +21,9 @@ banner_rect = banner.get_rect()
 banner_rect.x = 50
 banner_rect.y = 50
 
-#charger notre bouton
-play_button = pygame.image.load('img/interface/button.png')
-play_button = pygame.transform.scale(play_button, (400, 150))
+#charger nos boutons
+play_button = pygame.image.load('img/interface/jouer.gif')
+play_button = pygame.transform.scale(play_button, (200, 100))
 play_button_rect = play_button.get_rect()
 play_button_rect.x = screen.get_width() / 2
 play_button_rect.y = 50
@@ -31,14 +31,18 @@ play_button_rect.y = 50
 play_button_menu = pygame.image.load('img/interface/button_menu.png')
 play_button_menu = pygame.transform.scale(play_button_menu, (200, 75))
 play_button_menu_rect = play_button_menu.get_rect()
-play_button_menu_rect.x = 200
-play_button_menu_rect.y = 400
+play_button_menu_rect.x = screen.get_width() /2
+play_button_menu_rect.y = 50
+
+
+
+
+
 
 blackScreen = pygame.Surface(SCREEN_SIZE)               
 blackScreen.fill((0,0,0))
 
 is_playing = False
-
 
 ######################################################################## Boucle Principale ################################################################################################################
 
@@ -49,16 +53,16 @@ while True :
 
         timer = myFont.render("timer: "+str(game.print_clock()), 1, (255,255,255))
         score = myFont.render("Score: "+str(game.totalScore), 1, (255,255,255))
-        play_button = pygame.transform.scale(play_button, (200, 75))
+        play_button = pygame.transform.scale(play_button, (200, 100))
         play_button_rect = play_button.get_rect()
-        play_button_rect.x = 200
-        play_button_rect.y = 400
+        play_button_rect.x = screen.get_width() /2
+        play_button_rect.y = 50
         while is_playing == False :
             screen.blit(blackScreen, (0,0))
             screen.blit(timer, (200,50))
             screen.blit(score, (200,70))
-            screen.blit(banner, (400,70))
-            screen.blit(play_button_menu, (200,400))
+            screen.blit(banner, (50,75))
+            screen.blit(play_button_menu, ((screen.get_width() / 2),50))
             pygame.display.flip()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT :
@@ -68,6 +72,7 @@ while True :
                     if play_button_menu_rect.collidepoint(event.pos):
                         #lancer le jeu
                         is_playing = True
+
     else : 
         #Tout ce qu'il y a à afficher dans le menu de démarrage 
         mainMenu(screen, blackScreen, play_button, banner)
