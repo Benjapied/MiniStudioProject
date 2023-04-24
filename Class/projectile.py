e8e03b581394201aa48130ed27c3ad3b96688404
Perlainpin <greboul@gaming.tech>
2023-04-24 16:19:40 +0200

    ennemi ready (no bomber)

--- Class/projectile.py
+++ Class/projectile.py
@@ -9,18 +9,54 @@ class Projectile(pygame.sprite.Sprite):
         self.velocity = 5
         self.player = player
         self.color = color
+        self.animationDuration = 500
         #Image et position
-        self.image = pygame.image.load('img/player/projectiles/projectile_'+ self.color+'.png')
-        self.image = pygame.transform.scale(self.image, (50, 50))
+        self.image = pygame.image.load('img/player/projectiles/vfx.png')
+        if self.color == "neutral" :
+            self.animeStat = 0 #Numero du sprite de l'animation
+            self.listSprite = [] #Liste qui va contenir toutes les frames de l'animation
+            self.listSprite.append(self.image.subsurface(107, 103, 106, 67)) #Subsurface va prendre une partie de la sprite sheet
+            self.listSprite.append(self.image.subsurface(259, 105, 106, 67))
+            self.listSprite.append(self.image.subsurface(418, 103, 106, 67))
+            self.listSprite.append(self.image.subsurface(594, 105, 106, 67))
+            for i in range(4):
+                self.listSprite[i] = pygame.transform.scale(self.listSprite[i], (50, 30))
+                self.listSprite[i] = pygame.transform.flip(self.listSprite[i], True, False)
+            self.image = self.listSprite[self.animeStat]
+        elif self.color == "red" or self.color == "blue" or self.color == "yellow" or self.color == "green" :
+            self.listSprite = [] #liste des 4 différents sprites possibles
+            self.listSprite.append(self.image.subsurface(199,478,40,8))
+            self.listSprite.append(self.image.subsurface(199,503,40,8))
+            self.listSprite.append(self.image.subsurface(199,529,40,8))
+            self.listSprite.append(self.image.subsurface(202,552,40,8))
+            for i in range(4) :
+                self.listSprite[i] = pygame.transform.scale(self.listSprite[i],(50,10))
+            if self.color == "blue" :
+                self.image = self.listSprite[0]
+            elif self.color == "red" :
+                self.image = self.listSprite[1]
+            elif self.color == "green" :
+                self.image = self.listSprite[2]
+            elif self.color == "yellow" :
+                self.image = self.listSprite[3]
         self.rect = self.image.get_rect()
         self.rect.x = player.rect.x + 85
-        self.rect.y = player.rect.y + 5
+        self.rect.y = player.rect.y + 30
+
+    def animation (self) :
+        '''Fonction d'animation des projectiles qui pointent vers la gauche'''
+        self.animeStat = int((self.game.clock%self.animationDuration)/self.animationDuration*4) #Définition de l'image à afficher en fonction de la clock du jeu (si vous comprenez pas demandez à peter)
+        self.image = self.listSprite[self.animeStat]
 
     def remove(self):
         '''retire l'objet de la liste des projectiles'''
         self.player.all_projectiles.remove(self)
 
-
+    def animation (self) :
+        '''Fonction d'animation des projectiles qui pointent vers la gauche'''
+        self.animeStat = int((self.game.clock%self.animationDuration)/self.animationDuration*4) #Définition de l'image à afficher en fonction de la clock du jeu (si vous comprenez pas demandez à peter)
+        self.image = self.listSprite[self.animeStat]
+        
     def move(self):
         self.rect.x += self.velocity
 
@@ -71,12 +107,21 @@ class Simple_ennemi_projectile(pygame.sprite.Sprite):
         self.ennemi = ennemi
         self.velocity = 5
         self.direction = "left"
+        self.animationDuration = 500
         #Image et position
-        self.image = pygame.image.load('img/player/projectiles/projectile_neutral.png')
-        self.image = pygame.transform.scale(self.image, (50, 50))
+        self.image = pygame.image.load('img/player/projectiles/vfx.png')
+        self.animeStat = 0 #Numero du sprite de l'animation
+        self.listSprite = [] #Liste qui va contenir toutes les frames de l'animation
+        self.listSprite.append(self.image.subsurface(107, 103, 106, 67)) #Subsurface va prendre une partie de la sprite sheet
+        self.listSprite.append(self.image.subsurface(259, 105, 106, 67))
+        self.listSprite.append(self.image.subsurface(418, 103, 106, 67))
+        self.listSprite.append(self.image.subsurface(594, 105, 106, 67))
+        for i in range(4):
+            self.listSprite[i] = pygame.transform.scale(self.listSprite[i], (50, 30))
+        self.image = self.listSprite[self.animeStat]
         self.rect = self.image.get_rect()
-        self.rect.x = self.ennemi.rect.x 
-        self.rect.y = self.ennemi.rect.y 
+        self.rect.x = self.ennemi.rect.x
+        self.rect.y = self.ennemi.rect.y + (self.ennemi.rect.h / 3 )
 
 
     def remove_ennemi(self):
@@ -94,12 +139,16 @@ class Simple_ennemi_projectile(pygame.sprite.Sprite):
         if self.rect.x < 0:
             #supprimer le projectile
             self.remove_ennemi()
-            
+    
+    def animation (self) :
+        '''Fonction d'animation des projectiles qui pointent vers la gauche'''
+        self.animeStat = int((self.game.clock%self.animationDuration)/self.animationDuration*4) #Définition de l'image à afficher en fonction de la clock du jeu (si vous comprenez pas demandez à peter)
+        self.image = self.listSprite[self.animeStat]    
         
     def lunch_projec(self):
         '''Fonction qui crée un projectile ennemi et le place dans la liste des projec ennemis'''
         self.game.all_projectiles.add(self)
-
+        
 class Up_ennemi_projectile(Simple_ennemi_projectile):
     '''Class enfant de la classe projectile
     Classe qui créer un objet projectile lancés par les ennemis
